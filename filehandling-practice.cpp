eb3ca631f2d5977a90b486ac812f0957e915cc03
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2025-04-14 03:11:34 +0500

    message test to be commited

--- filehandling-practice.cpp
+++ filehandling-practice.cpp
@@ -1,42 +1,91 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-
 using namespace std;
 
-int main()
-{
-	string line;
-	int num, count=0, sum=0;
-
-	ifstream getfile("integer.txt");
-	if (!getfile){
-		cerr << "Error opening file" << endl;
-		return 1;
-
-	}
-	while (getfile.is_open()) {
-		//num = getfile.get(); testing 
-		getfile >> num;
-		sum += num;
-		count++;
-
-	}
-	int avg = sum / count;
-	ofstream writefile("result.txt");
+int main() {
+	const int n = 10;
+	int arr1[n], num;
+	ifstream getfile("data.txt");
 	
-	if (writefile.fail()){
-		cerr << "Error writing file content in result.txt." << endl;
+	if (!getfile.is_open()) {
+		cerr << "Error reading data from file" << endl;
 		return 1;
-
-	}
-	else {
-		writefile << "The sum is: " << sum << endl << "The avg is: " << avg;
-		cout << "Content written in the file successfully." << endl;
 	}
 
+	for (int i = 0; i < n; i++)
+	{
+		getfile >> arr[i];
 
+	}
+	for (int i = 0; i < n-1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (arr[j + 1] > arr[j]) {
+				swap(arr[j + 1], arr[j]);
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << endl;
+	}
 
 	return 0;
-
 }
+
+//int main()
+//{
+//	string line;
+//	int num, count=0, sum=0;
+//	cout << "check" << endl;
+//	ifstream getfile("integer.txt");
+//	if (!getfile){
+//		cerr << "Error opening file" << endl;
+//		return 1;
+//
+//	}
+//	while (getfile >> num) {
+//		//num = getfile.get(); testing 
+//		sum += num;
+//		count++;
+//
+//	}
+//	int avg = sum / count;
+//	ofstream writefile("result.txt");
+//	
+//	if (writefile.fail()){
+//		cerr << "Error writing file content in result.txt." << endl;
+//		return 1;
+//
+//	}
+//	else {
+//		writefile << "The sum is: " << sum << endl << "The avg is: " << avg;
+//		cout << "Content written in the file successfully." << endl;
+//	}
+//
+//
+//
+//	return 0;
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
